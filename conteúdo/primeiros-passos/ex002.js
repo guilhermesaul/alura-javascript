f987ccc79d15389df70069e513e19e75b5c14da2
guisaul <guilhermesaul190@gmail.com>
2025-03-03 21:35:47 -0300

    Trabalhando com variáveis

---
+++ conteúdo/primeiros-passos/ex002.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com variáveis")
+//Console.log("Trabalhando com variáveis")
+//JS é case-sensitive = sensível a letras maiúsculas e minúsculas
+
+const idade = 29
+const nome = "Ricardo"
+
+//não fazer
+//mes = "Janeiro"
+
+console.log(idade)
+console.log(nome)
