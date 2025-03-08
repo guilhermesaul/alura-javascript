f4a69384e6508cbb261e35d8ed04c584a1d99385
guisaul <guilhermesaul190@gmail.com>
2025-03-07 21:53:15 -0300

    mudando valor de variaveis

---
+++ conteúdo/primeiros-passos/ex005.js
@@ -0,0 +1,11 @@
+console.log("Trabalhando com Atribuição de variáveis")
+const idade = 29
+const primeiroNome = "Ricardo"
+const sobrenome = "Bugan"
+
+//console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+nomeCompleto = primeiroNome + " " + sobrenome
+console.log(nomeCompleto)
