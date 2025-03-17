68a7591f43bd34623d001da3b034184ba095523a
guisaul <guilhermesaul190@gmail.com>
2025-03-17 20:19:33 -0300

    Trabalhando com listas

---
+++ conteúdo/primeiros-passos/ex006.js
@@ -0,0 +1,14 @@
+console.log(`Trabalhando com listas`)
+// const salvador = `Salvador`
+// const saopaulo = `São Paulo`
+// const riodejaneiro = `Rio de Janeiro`
+const listadedestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+)
+listadedestinos.push(`Curitiba`) // Adicionando um item na lista
+
+console.log(`Destinos possíveis`)
+// console.log(salvador, saopaulo, riodejaneiro)
+console.log(listadedestinos)
